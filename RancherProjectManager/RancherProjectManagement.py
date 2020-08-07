47109b21dc305a0d93704b53893733dfb1e0969c
Tyler Evert <tevert@motus.com>
2020-08-07 16:27:15 -0500

    Use actual logging instead of prints

--- RancherProjectManager/RancherProjectManagement.py
+++ RancherProjectManager/RancherProjectManagement.py
@@ -1,5 +1,6 @@
 from kubernetes import client, config, watch
 import requests
+import logging
 import os
 from .RancherApi import RancherResponseError
 
@@ -18,24 +19,22 @@ class RancherProjectManagement:
 
     def watch(self):
         # Check 'em all at startup
-        print("Checking all namespaces")
+        logging.info("Checking all namespaces")
         namespaces = self.kubeapi.list_namespace()
         for ns in namespaces.items:
             self.process_namespace(ns)
 
         # Watch for more changes going forward
-        print("Watching for additional namespace changes")
+        logging.info("Watching for additional namespace changes")
         watcher = watch.Watch()
         for ns_event in watcher.stream(self.kubeapi.list_namespace):
             try:
                 if ns_event['type'] == 'MODIFIED':
                     self.process_namespace(ns_event['object'])
             except (requests.HTTPError, RancherResponseError, ValueError, KeyError) as e:
-                print("ERROR processing namespace event - raw event: " + str(ns_event))
-                print(e)
+                logging.exception("ERROR processing namespace event - raw event: " + str(ns_event))
             except Exception as e:
-                print("FATAL ERROR processing namespace event - raw event: " + str(ns_event))
-                print(e)
+                logging.exception("FATAL ERROR processing namespace event - raw event: " + str(ns_event))
                 raise
 
     def process_namespace(self, namespace):
@@ -50,7 +49,7 @@ class RancherProjectManagement:
 
         # Create the rancher project if necessary
         if project is None:
-            print(f'Namespace {namespace.metadata.name} requested project named {project_name} which didn\'t exist, creating now')
+            logging.info(f'Namespace {namespace.metadata.name} requested project named {project_name} which didn\'t exist, creating now')
 
             # Check if there's a special cluster we're supposed to use
             cluster = self.default_cluster
@@ -65,6 +64,6 @@ class RancherProjectManagement:
             return
 
         # Patch the project ID on there
-        print(f'Annotating namespace {namespace.metadata.name} for requested project named {project_name} with its ID {project_id}')
+        logging.info(f'Annotating namespace {namespace.metadata.name} for requested project named {project_name} with its ID {project_id}')
         annotations[self.project_id_annotation] = project_id
         self.kubeapi.patch_namespace(namespace.metadata.name, namespace)
